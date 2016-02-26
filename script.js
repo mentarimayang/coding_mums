5bda7e6aa8dbab70a78577bd4e7298fbb79e217e
mayang <mentari_mayang@yahoo.com>
2016-02-26 11:33:20 +0700

    day2b
    pusing

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿function readMore(){
+		var temp=document.getElementById(readMore).style.display;
+		if(temp=="none")
+			document.getElementById("readMore").style.display="block";
+		else
+			document.getElementById("readMore").style.display="none";
+		}	
